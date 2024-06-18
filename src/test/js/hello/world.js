e85cefc87297884d8460122f3bfa2798ce2c991c
johro35 <john.roche@vgregion.se>
2024-06-18 11:02:07 +0200

    Moving to spring-boot-init setup

--- src/test/js/hello/world.js
+++
@@ -1,9 +0,0 @@
-function process(method, requestHeaders, responseHeaders, request, response) {
-    print(requestHeaders.values());
-    responseHeaders.add("hey", "ho");
-
-    print(request);
-    response.write(method + "echo: " + request);
-    return 200;
-}
-
